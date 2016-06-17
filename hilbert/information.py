dab658a6495d5abf25506b6e56891803226073b7
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2016-06-17 12:01:45 +0100

    n file

--- hilbert/information.py
+++
@@ -1,8 +0,0 @@
-#!/usr/pkg/bin/env python
-"""
-Get information for hilbmetric
-"""
-
-__description__ = "Python program for the Hilbert projective metric"
-__version__ = "1.0-pre01"
-__url__ = "https://github.com/kiaderouiche/hilbmetric"
