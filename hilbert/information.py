f083fc0acd2aca3cca4e46ebf11aa2aabe740756
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2016-06-17 11:37:12 +0100

    ident, lint, change code and doc

--- hilbert/information.py
+++ hilbert/information.py
@@ -1,7 +1,8 @@
 #!/usr/pkg/bin/env python
-#
-#
+"""
+Get information for hilbmetric
+"""
 
-__description__ = "Python program for the Hilbert projective metric" 
-__version__ = "1.0-current01"
+__description__ = "Python program for the Hilbert projective metric"
+__version__ = "1.0-pre01"
 __url__ = "https://github.com/kiaderouiche/hilbmetric"
