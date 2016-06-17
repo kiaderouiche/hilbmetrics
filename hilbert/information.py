4c8b6549a83c9bd4926400cf8a659975c7ae88a2
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2016-06-17 04:13:16 +0100

    update

--- hilbert/information.py
+++ hilbert/information.py
@@ -3,5 +3,5 @@
 #
 
 __description__ = "Python program for the Hilbert projective metric" 
-__version__ = "0.0.1"
-__url_proj__ = "https://github.com/kiaderouiche/hilbmetric"
+__version__ = "1.0-current01"
+__url__ = "https://github.com/kiaderouiche/hilbmetric"
