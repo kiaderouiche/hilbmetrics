18bdd1ef0095794a35a1d44079d54bf7e3a2edc6
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2016-06-26 22:46:47 +0100

    modiff

--- hilbert/common/__appinfo__.py
+++ hilbert/common/__appinfo__.py
@@ -6,3 +6,21 @@ Get information for hilbmetric
 __description__ = "Python program for the Hilbert projective metric"
 __version__ = "1.0-pre01"
 __url__ = "https://github.com/kiaderouiche/hilbmetric"
+__email__= "kamel.derouiche@gmail.com"
+__keywords__= ['hilbert, geometry', 'mesure', 'convex']
+
+__classifiers__=[
+         'Development Status :: 3 - Alpha',
+         'Environment :: Other Environment',
+         'Intended Audience :: Education',
+         'Intended Audience :: Developers',
+         'Intended Audience :: Science/Research',
+         'License :: OSI Approved :: Apache',
+         'Natural Language :: English',
+         'Operating System :: OS Independent',
+         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3',
+         'Topic :: Scientific/Engineering :: Mathematics',
+         'Topic :: Scientific/Engineering :: Visualization',
+],
