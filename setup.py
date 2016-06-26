c4192671c53c539bd0651fa50b96bd04f065d06c
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2016-06-26 22:31:09 +0100

    manip

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ from platform import architecture
 
 from setuptools import setup
 
-from hilbert.information import __description__, __version__, __url_proj__ 
-
-email=['kamel.derouiche@gmail.com']
+from hilbert.common.__appinfo__ import __description__, \
+    __version__, __url_proj__, __email__ 
 
 with open('README') as file:
        long_description= file.read()
@@ -36,35 +35,35 @@ def install_data_files():
         data_files = []
     return data_files
         
-setup(
-    name='hilbmetric',
-    version=__version__,
-    description=__description__,
-    long_description=long_description,
-    authors='HilbertMetric Developers',
-    author_email=email,
-    maintainer = 'HilbertMetric Developers',
-    maintainer_email = email,
-    url=__url_proj__,
-    keywords=['hilbert, geometry', 'mesure', 'convex'],
-    py_modules=['hilbmetric'],
-    license='Apache-2.0',
-    data_files=install_data_files(),
-    install_requires=["sympy(>=0.7.6.1)", "PyQt4 (>=4.4)"],
-    platforms='any',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Other Environment',
-        'Intended Audience :: Education',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Scientific/Engineering :: Visualization',
-    ],
-)
+# setup(
+#     name='hilbmetric',
+#     version=__version__,
+#     description=__description__,
+#     long_description=long_description,
+#     authors='HilbertMetric Developers',
+#     author_email=__email__,
+#     maintainer = 'HilbertMetric Developers',
+#     maintainer_email =__email__,
+#     url=__url_proj__,
+#     keywords=['hilbert, geometry', 'mesure', 'convex'],
+#     py_modules=['hilbmetric'],
+#     license='Apache-2.0',
+#     data_files=install_data_files(),
+#     install_requires=["sympy(>=0.7.6.1)", "PyQt4 (>=4.4)"],
+#     platforms='any',
+#     classifiers=[
+#         'Development Status :: 3 - Alpha',
+#         'Environment :: Other Environment',
+#         'Intended Audience :: Education',
+#         'Intended Audience :: Developers',
+#         'Intended Audience :: Science/Research',
+#         'License :: OSI Approved :: Apache',
+#         'Natural Language :: English',
+#         'Operating System :: OS Independent',
+#         'Programming Language :: Python :: 2.7',
+#         'Programming Language :: Python :: 3',
+#         'Programming Language :: Python :: 3',
+#         'Topic :: Scientific/Engineering :: Mathematics',
+#         'Topic :: Scientific/Engineering :: Visualization',
+#     ],
+# )
