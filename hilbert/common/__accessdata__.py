bc950feedf03abdc9eef1a7bad7320eaa6c9c0b7
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2016-06-28 02:40:32 +0100

    ident script

--- hilbert/common/__accessdata__.py
+++ hilbert/common/__accessdata__.py
@@ -6,22 +6,20 @@ PY3 = sys.version_info[0] == 3
 
 
 def install_data_files():
-    
+
     if sys.platform.startswith('netbsd'):
-        
         pass
     elif sys.platform.startswith('freebsd'):
-        
         pass
     elif sys.platform.startswith('linux'):
         if PY3:
             data_files = [('share/applications', ['script/hilbmetric.desktop']),
                           ('share/pixmaps', ['data/hilbmetric.png'])]
         else:
-            data_files = [('share/applications', ['script/hilmetric.desktop'] ), 
+            data_files = [('share/applications', ['script/hilmetric.desktop'] ),
                           ('share/pixmaps', ['data/hilbmetric.png'])]
     elif os.name =='nt':
-            data_files = [('script', ['data/hilbmetric.ico'])]
+        data_files = [('script', ['data/hilbmetric.ico'])]
     else:
-        dtfiles = []
-    return datafiles
+        data_files = []
+    return data_files
