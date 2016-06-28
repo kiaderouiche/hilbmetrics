0a7e462b28c328b009333b992ee0277ea2746819
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2016-06-28 02:49:19 +0100

    add path netbsd and freebsd

--- hilbert/common/__accessdata__.py
+++ hilbert/common/__accessdata__.py
@@ -6,11 +6,12 @@ PY3 = sys.version_info[0] == 3
 
 
 def install_data_files():
-
     if sys.platform.startswith('netbsd'):
-        pass
+        data_files = [('/usr/pkg/share/applications', ['script/hilbmetric.desktop']),
+                          ('/usr/pkg/share/pixmaps', ['data/hilbmetric.png'])]
     elif sys.platform.startswith('freebsd'):
-        pass
+        data_files = [('/usr/local/share/applications', ['script/hilbmetric.desktop']),
+                          ('/usr/local/share/pixmaps', ['data/hilbmetric.png'])]
     elif sys.platform.startswith('linux'):
         if PY3:
             data_files = [('share/applications', ['script/hilbmetric.desktop']),
