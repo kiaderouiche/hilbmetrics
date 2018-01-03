05ae48601872d7a00150a7c2eaae22e4fe78f251
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2018-01-03 16:55:50 +0100

    +new, modif

---
+++ hilbert/hilbert.py
@@ -0,0 +1,15 @@
+"""
+
+Hilbert projective metric
+=========================
+
+- boundary of Ω
+- order of the points p,x, q, y
+
+              |py||xq|         
+d(x,y) = ln( ---------- )
+              |px||yq|
+
+"""
+
+from sympy import log
--- hilbert/init_app.py
+++ hilbert/init_app.py
@@ -1,5 +1,5 @@
-#!/usr/pkg/bin/env python3
-#
+"""
+"""
 
 import os
 import sys
