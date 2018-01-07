5d082225354ff708d9454dcbd08e2aee07259a10
K.I.A Derouiche <kamel.derouiche@gmail.com>
2018-01-07 02:24:33 +0100

    Update hilbert.py

--- hilbert/hilbert.py
+++ hilbert/hilbert.py
@@ -5,11 +5,23 @@ Hilbert projective metric
 
 - boundary of Ω
 - order of the points p,x, q, y
+- || || norm
 
-              |py||xq|         
-d(x,y) = ln( ---------- )
-              |px||yq|
+              ||py|| ||xq||         
+d(x,y) = ln( ---------------- )
+              ||px|| ||yq||
 
 """
 
+from sympy import symbols
 from sympy import log
+
+x, y = symbols('x, y')
+p, q = symbols('p, q')
+
+def Norm():
+  pass
+
+def d(x, y):
+  pass 
+              
