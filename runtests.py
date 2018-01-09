599c757c28563eb79018dd62e7b45d85a61e45ca
K.I.A Derouiche <kamel.derouiche@gmail.com>
2018-01-09 01:02:50 +0100

    Update runtests.py

--- runtests.py
+++ runtests.py
@@ -2,3 +2,12 @@ import sys
 import unittest
 import warnings
 
+
+def runtests(argv):
+  # obtain command line arguments
+  try:
+    opts, args = getopt.getopt(argv, 'hvp:m')
+  except getopt.GetoptError:
+    usage()
+    sys.exit(2)
+
