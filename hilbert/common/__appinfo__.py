8b7bcd079b3ed4f3f78629059313421c9c32f1ec
K.I.A Derouiche <kamel.derouiche@gmail.com>
2019-06-13 13:52:18 +0100

    Update __appinfo__.py

--- hilbert/common/__appinfo__.py
+++ hilbert/common/__appinfo__.py
@@ -5,6 +5,7 @@ Get information for hilbmetric
 from hilbert.common.__accessdata__ import install_data_files
 
 __install_requires__ = [
+    'PyQt5',
     'sympy>= 1.1',
     'opencv-python>= 3.4',
     'scipy>= 1.0.0',
@@ -19,7 +20,7 @@ __platform__ = "any"
 __data_file__ = install_data_files
 __url__ = "https://github.com/kiaderouiche/hilbmetric"
 __email__= "kamel.derouiche@gmail.com"
-__keywords__= ['hilbert, geometry', 'mesure', 'convex', 'manifold', 'group', 'hyperbolic', 'birapport', 'ln']
+__keywords__= ['hilbert, geometry', 'mesure', 'convex', 'manifold', 'hyperbolic', 'birapport', 'ln', 'hull']
 __classifiers__= [
          'Development Status :: 3 - Alpha',
          'Environment :: Other Environment',
@@ -29,7 +30,6 @@ __classifiers__= [
          'License :: OSI Approved :: Apache',
          'Natural Language :: English',
          'Operating System :: OS Independent',
-         'Programming Language :: Python :: 3',
          'Programming Language :: Python :: 3',
          'Topic :: Scientific/Engineering :: Mathematics',
          'Topic :: Scientific/Engineering :: Visualization',
